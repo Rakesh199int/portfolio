aec22641f524d274a31bb6c76b605f0733583317
Rakesh199int <rakeshpuppala1999int@gmail.com>
2024-09-21 17:24:00 -0700

    Update Experience.js

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -9,16 +9,7 @@ export const Experience = () => {
         <h2 className="experience-heading">Experience</h2>
         <Row>
           <Col md={6}>
-            <div className="experience-item">
-              <img className="experience-logo" src={companyLogo1} alt="Company Logo" />
-              <h3 className="experience-title">SAP SD Consultant</h3>
-              <p className="experience-company">Infosys</p>
-              <p className="experience-dates">Nov 2020  - July 2022</p>
-              <ul className="experience-details">
-                <li className="experience-detail">Configured end-to-end SD setups, including enterprise structure maintenance, document type controls, item category determination, shipping point determination, output determination, plant determination, route determination, copy controls, batch job setups, and IDOC processing. </li>
-                <li className="experience-detail">Implemented critical project deliverables within ServiceNow to facilitate a seamless production launch and provided post-go-live support for uninterrupted operations.</li>
-              </ul>
-            </div>
+            
           </Col>
           <Col md={6}>
             <div className="experience-item1">
